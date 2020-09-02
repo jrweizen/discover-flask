cad0b737e66883557c816469b545eae77cf32a76
jrweizen <jrweizen@gmail.com>
2020-09-02 19:44:43 -0300

    first endpoints

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Test for Echo!"
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
