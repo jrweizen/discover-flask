a93d092877717c4b1c02bec6e149b71344c14113
jrweizen <jrweizen@gmail.com>
2020-09-06 11:03:30 -0300

    login and logout features with messaging

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
+# Create the application object
 app = Flask(__name__)
 
 app.secret_key = "my precious"
 
+# Use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Index page - Test for Echo!"
+    # return "Index page - Test for Echo!"
+    return render_template('index.html')
 
 @app.route('/welcome')
 def welcome():
@@ -21,14 +25,17 @@ def login():
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
+            flash('You were just logged in!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    flash('You were just logged out!')
     return redirect(url_for('welcome'))
 
+# Start the server with the run() method
 if __name__ == '__main__':
     app.run(debug=True)
 
